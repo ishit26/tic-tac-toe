274e888019019b44c86250a5fd2a72b21a25fce6
Ishit Chauhan <ishit.chauhan26@gmail.com>
2023-11-09 19:52:37 -0500

    Added README.md file, starter file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tic-tac-toe
+
+A simple console based game using python where player is able to select between
+'X' or 'O' and these will be placed in its correct position using the number pad of 
+the keyboard input.
---
+++ tic-tac-toe.py
@@ -0,0 +1,9 @@
+#TODO: code to display board
+#TODO: code to choose marker 'X' or 'O'
+#TODO: code to position the marker choice in the board
+#TODO: code to which player should start game player 1 or player 2
+#TODO: code to check the win condition
+#TODO: code to check available space on board after player put his marker 
+#TODO: code to check weather board is full or not(Draw Condition)
+#TODO: code to take input from player to put the marker on board using keyboard numpad
+#TODO: code to if palyer want to play the game again
